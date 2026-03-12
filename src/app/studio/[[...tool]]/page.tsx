b5e29b6b78c7663dd66c40834ce66f8c48929768
Besart Olluri <bess@Besarts-MacBook-Air.local>
2026-03-13 00:34:57 +0100

    Fix studio page: use dynamic import with ssr:false

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,7 +1,10 @@
-'use client'
-
-import { NextStudio } from 'next-sanity/studio'
+import dynamic from 'next/dynamic'
 import sanityConfig from '../../../../sanity.config'
+
+const NextStudio = dynamic(
+  () => import('next-sanity/studio').then((mod) => mod.NextStudio),
+  { ssr: false }
+)
 
 export default function StudioPage() {
   return <NextStudio config={sanityConfig} />
